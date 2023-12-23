1af6236643b6888648ec7f614aa12d904faef523
Saakshi Kobarne <115942356+saakshikobarne@users.noreply.github.com>
2023-12-23 19:30:36 +0530

    Create 03. Repetitions.cpp

---
+++ 0. Introductory Problems/03. Repetitions.cpp
@@ -0,0 +1,101 @@
+/*
+Problem Statement:
+You are given a DNA sequence: a string consisting of characters A, C, G, and T. Your task is to find the longest 
+repetition in the sequence. This is a maximum-length substring containing only one type of character.
+
+Input
+The only input line contains a string of n characters.
+Output
+Print one integer: the length of the longest repetition.
+Constraints
+1 <= n <= 10^6
+
+Example
+Input:
+ATTCGGGA
+ 
+Output:
+3
+*/
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pb push_back
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define fr(i,a,b) for(int i=a; i<b; i++)
+#define loop(x,n) for(int x=0; x<n; ++x)
+#define mod 1000000007
+#define inf (1LL<<60)
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+
+void precal(){
+
+}
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+#define ll long long
+#define pb push_back
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define fr(i,a,b) for(int i=a; i<b; i++)
+#define loop(x,n) for(int x=0; x<n; ++x)
+#define mod 1000000007
+#define inf (1LL<<60)
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+
+void precal(){
+
+}
+
+void solve() {
+	string s;
+	cin>>s;
+	
+	ll n=1, maxi=0;
+  
+	loop(i, s.length()){
+		if(s[i]==s[i+1]) n++;
+		else n=1;
+		maxi= max(n, maxi);
+	}
+	cout<<maxi;
+}
+
+int main(){
+
+  fast_io;
+  cout<<fixed;
+  cout<< setprecision(10);
+  precal();
+  int t=1;
+  //cin >>t;
+  for(int i=1; i<=t; i++){
+    //cout<< "Case #" << i << ": ";
+    solve();
+  }
+
+  return 0;
+}
+
+int main(){
+
+  fast_io;
+  cout<<fixed;
+  cout<< setprecision(10);
+  precal();
+  int t=1;
+  //cin >>t;
+  for(int i=1; i<=t; i++){
+    //cout<< "Case #" << i << ": ";
+    solve();
+  }
+
+  return 0;
+}
